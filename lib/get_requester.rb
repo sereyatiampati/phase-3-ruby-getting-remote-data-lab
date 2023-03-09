e04e280041d0833a61e1e4ef043ed4a38d0bfa07
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-09 09:24:07 +0300

    working with data requested from an API

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+    def initialize(url)
+        @url=url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        parsed_data = JSON.parse(self.get_response_body)
+        parsed_data.collect do|person|
+            person["name"]
+        end
+    end
+end
